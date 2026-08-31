1b6f49aa0485cae5661f63b657f8397399cda5f6
DhabalenduS <73298060+DhabalenduS@users.noreply.github.com>
2026-08-31 14:41:11 +0530

    Update app.py

--- clip_01_eigenvalues/app.py
+++ clip_01_eigenvalues/app.py
@@ -9,7 +9,7 @@ import streamlit as st
 # ------------------------------------------------------------
 
 st.set_page_config(
-    page_title="The Essence of Eigenvalues and Eigenvectors",
+    page_title="Welcome to The Essence of Eigenvalues and Eigenvectors",
     page_icon="📐",
     layout="wide",
     initial_sidebar_state="collapsed",
