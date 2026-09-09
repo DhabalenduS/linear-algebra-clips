575861de1759cd18e99092ae32da48ea5c25983d
DhabalenduS <73298060+DhabalenduS@users.noreply.github.com>
2026-09-09 16:10:20 +0530

    Update app.py

--- clip_01_eigenvalues/app.py
+++ clip_01_eigenvalues/app.py
@@ -629,9 +629,9 @@ elif 8 <= st.session_state.presentation_state <= 18:
 
                     // 3. White Ball Base Shell
                     const ballGeo = new THREE.SphereGeometry(
-                        R*0.5, 
-                        70, 
-                        70, 
+                        R, 
+                        R*0.8, 
+                        R*0.8, 
                         sphereStart, 
                         sphereArc
                     );
