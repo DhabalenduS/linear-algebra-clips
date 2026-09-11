6cdedba52b8f6a90acd5fd7894675c16724fb636
DhabalenduS <73298060+DhabalenduS@users.noreply.github.com>
2026-09-11 19:07:40 +0530

    Update app.py

--- clip_01_eigenvalues/app.py
+++ clip_01_eigenvalues/app.py
@@ -815,11 +815,11 @@ elif 8 <= st.session_state.presentation_state <= 18:
                     pSphere.position.copy(pLocal);
                     ballGroup.add(pSphere);
 
-                    const pLabel = makeMathTextSprite("P (x, y, z)", "#06b6d4");
+                    const pLabel = makeMathTextSprite("P (x, y, z)", "#0284c7");
                     pLabel.scale.set(1.4, 0.44, 1);
                     pLabel.position.copy(pLocal).add(new THREE.Vector3(0.72, 0.32, 0.0));
                     ballGroup.add(pLabel);
-
+                    
                     if (currentState === 11) {{
                         const cTopGeo = new THREE.SphereGeometry(0.09, 32, 32);
                         const cTopMat = new THREE.MeshStandardMaterial({{
