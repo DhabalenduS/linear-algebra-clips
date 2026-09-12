c728f5b3a8e14fef1b919fd0a23fb65939f95e5c
DhabalenduS <73298060+DhabalenduS@users.noreply.github.com>
2026-09-12 17:35:22 +0530

    Update app.py

--- clip_01_eigenvalues/app.py
+++ clip_01_eigenvalues/app.py
@@ -482,9 +482,9 @@ elif 8 <= st.session_state.presentation_state <= 25:
                 scene.add(rimLight);
                
                 // Dedicated Bottom Bounce Light (Brightens the underside)
-                const bottomFillLight = new THREE.DirectionalLight(0xffffff, 0.50);
-                bottomFillLight.position.set(0, -10, 12);
-                scene.add(bottomFillLight);
+                //const bottomFillLight = new THREE.DirectionalLight(0xffffff, 0.50);
+                //bottomFillLight.position.set(0, -10, 12);
+                //scene.add(bottomFillLight);
 
                 function createContactShadowTexture() {{
                     const sCanvas = document.createElement('canvas');
